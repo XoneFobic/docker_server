ed2a6efd23157e4a059e2371849f272ec2f3f294
XoneFobic <xonefobic@derp.af>
2019-11-15 02:15:00 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Production and Development environment for DeRP projects
+
+#### Losely based on Laradock.io
---
+++ workspace/files/aliases.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+alias phpunit="./vendor/bin/phpunit"
+alias pu="phpunit"
+alias puf="phpunit --filter"
+alias pud='phpunit --debug'
